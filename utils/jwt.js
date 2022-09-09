f9e48a52bc69230c1a553660ff12704e4cd3e570
Sergey_Stav <sergl2509@gmail.com>
2022-09-09 20:05:16 +0300

    new

--- utils/jwt.js
+++ utils/jwt.js
@@ -1,6 +1,6 @@
-const { NODE_ENV, JWT_SECRET } = process.env;
-
 const jwt = require('jsonwebtoken');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
 
 const SECRET_KEY_DEV = 'some-secret-key';
 const getJwtToken = (id) => jwt.sign({ _id: id }, NODE_ENV === 'production' ? JWT_SECRET : SECRET_KEY_DEV, { expiresIn: '7d' });
