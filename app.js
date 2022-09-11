994f6b5c463fc6a12523283372e6b04be0e5eb09
Sergey_Stav <sergl2509@gmail.com>
2022-09-11 18:24:36 +0300

    ПР15

--- app.js
+++ app.js
@@ -43,6 +43,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
